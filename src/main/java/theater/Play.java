555d915d3cc177de5ae944c9b11d36ffdbf8fa9f
samiwal1a <s.walia@mail.utoronto.ca>
2025-11-24 16:29:34 -0500

    Add files via upload
    checkstyle changes

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,36 @@
 package theater;
 
+/**
+ * Represents a Play object.
+ *
+ * @null This class does not return null unless explicitly documented.
+ * @return No return value for this class.
+ */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
+
